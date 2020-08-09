abbbf46efe7dd2be39408c37a0f4d98e8b2d9248
Peter Kubov <peter.kubov@avast.com>
2020-08-09 14:15:38 +0200

    Provide documentation for data analysis console.

--- include/r2plugin/console/data_analysis.h
+++ include/r2plugin/console/data_analysis.h
@@ -5,23 +5,38 @@
 namespace retdec {
 namespace r2plugin {
 
+/**
+ * Provides and implements Data Analysis console interface
+ * that is shown as pdza_ command in r2.
+ */
 class DataAnalysisConsole: public Console {
 protected:
+	/// Protected constructor. DataAnalysisConsole is meant to be used as singleton.
 	DataAnalysisConsole();
 
 public:
+	/// Calls handle method of singleton.
 	static bool handleCommand(const std::string& commad, const R2InfoProvider& info);
+
+	/// Representation of pdza command.
 	static Console::Command AnalyzeRange;
+
+	/// Representation of pdzaa command.
 	static Console::Command AnalyzeWholeBinary;
 
 private:
+	/// Implementation of pdza command.
 	static bool analyzeRange(const std::string&, const R2InfoProvider& info);
+
+	/// Implementation of pdzaa command.
 	static bool analyzeWholeBinary(const std::string&, const R2InfoProvider& info);
 
 private:
+	/// Helper method. Parses arguments of pdza commnad.
 	static common::AddressRange parseRange(const std::string& range);
 
 private:
+	/// Singleton.
 	static DataAnalysisConsole console;
 };
 
